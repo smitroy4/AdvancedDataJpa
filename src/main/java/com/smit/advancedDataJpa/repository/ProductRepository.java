a02c449140c745d2db820e082e203e1482b376e8
Smit Roy <smitroy22@gmail.com>
2026-03-18 00:39:31 +0530

    4 new Test-cases created with 1 custom JPQL query

--- src/main/java/com/smit/advancedDataJpa/repository/ProductRepository.java
+++ src/main/java/com/smit/advancedDataJpa/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package com.smit.advancedDataJpa.repository;
 
 import com.smit.advancedDataJpa.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
@@ -16,4 +18,18 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByQuantityAndPrice(int i, BigDecimal bigDecimal);
 
     List<Product> findByQuantityOrPrice(int i, BigDecimal bigDecimal);
+
+    List<Product> findByQuantityGreaterThanAndPriceLessThan(int i, int i1);
+
+    List<Product> findByQuantityGreaterThanOrPriceLessThan(int i, int i1);
+
+    List<Product> findByTitleLike(String s);
+
+    List<Product> findByTitleContainingIgnoreCase(String choco);
+
+//    Optional<Product> findByTitleAndPrice(String laysChips, BigDecimal bigDecimal);
+
+    @Query("select e from Product e where e.title=?1 and e.price=?2")
+    Optional<Product> findByTitleAndPrice(String title, BigDecimal price);
+
 }
--- src/test/java/com/smit/advancedDataJpa/ApplicationTests.java
+++ src/test/java/com/smit/advancedDataJpa/ApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class ApplicationTests {
@@ -20,6 +21,7 @@ class ApplicationTests {
 	void contextLoads() {
 	}
 
+//    1
     @Test
     void testRepository(){
         Product product = Product.builder()
@@ -32,38 +34,78 @@ class ApplicationTests {
         Product savedProduct = productRepository.save(product);
         System.out.println(savedProduct);
     }
-
+//    2
     @Test
     void getRepository(){
         List<Product> productList = productRepository.findAll();
-        productList.stream().forEach(product -> System.out.println(product));
+        productList.forEach(product -> System.out.println(product));
     }
 
+//    3
     @Test
     void getRepositoryByTitle(){
         List<Product> productList = productRepository.findByTitle("Pepsi");
-        productList.stream().forEach(product -> System.out.println(product));
+        productList.forEach(System.out::println);
     }
 
+//    4
     @Test
     void getRepositoryByTime(){
         List<Product> productByTime = productRepository.findByCreatedAtAfter(
                 LocalDateTime.of(2024, 1, 1, 1, 0, 0)
         );
-        System.out.println(productByTime);
+        productByTime.forEach(System.out::println);
     }
 
+//    5
     @Test
     void getRepositoryByQtyAndPrice(){
         List<Product> productByQtyPrice = productRepository.findByQuantityAndPrice(4,BigDecimal.valueOf(10));
-        System.out.println(productByQtyPrice);
+        productByQtyPrice.forEach(System.out::println);
     }
 
+//    6
     @Test
     void getRepositoryByQtyOrPrice(){
         List<Product> productByQtyPrice = productRepository.findByQuantityOrPrice(20,BigDecimal.valueOf(30));
-        System.out.println(productByQtyPrice);
+        productByQtyPrice.forEach(System.out::println);
     }
+
+//    7
+    @Test
+    void getRepositoryQtyAndPriceFilters(){
+        List<Product> productFilteredByQtyAndPrice = productRepository.findByQuantityGreaterThanOrPriceLessThan(4,20);
+        productFilteredByQtyAndPrice.forEach(System.out::println);
+    }
+
+//    8
+    @Test
+    void getRepositoryByTitleLike(){
+        List<Product> productByTitleLike = productRepository.findByTitleLike("%si%");
+        productByTitleLike.forEach(System.out::println);
+    }
+
+//    9
+    @Test
+    void getRepositoryByContainingIgnoreCase(){
+        List<Product> productByContainingIgnoreCase = productRepository.findByTitleContainingIgnoreCase("Choco");
+        productByContainingIgnoreCase.forEach(System.out::println);
+    }
+
+//    10
+    @Test
+    void getSingleFromRepository(){
+        Optional<Product> product = productRepository.findByTitleAndPrice("Lays Chips", BigDecimal.valueOf(15));
+        product.ifPresent(System.out::println);
+
+    }
+
+
+
+
+
+
+
 
 
 }
